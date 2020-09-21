1d5c2b54d56ee2325072973304c7b6fa0df45839
Zhilong <zhilongjia@users.noreply.github.com>
2020-09-21 21:13:12 +0800

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -12,5 +12,5 @@ git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git boo
 cd book-output
 cp -r ../docs ./
 git add --all *
-git commit -m"Update the book `date`" || true
+git commit -m "Update the book at `TZ='Asia/Shanghai' date`" || true
 git push -q origin master
